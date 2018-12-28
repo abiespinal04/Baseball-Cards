3804bf03983ae0c9f2dca911aed5e0d1b401ecad
Abimael Espinal <abimaelespinal@hotmail.com>
2018-12-28 02:55:21 -0500

    Added the JOptionPane messagebox to display error for the button (insert) and added JTextArea

--- src/BaseballCards.java
+++ src/BaseballCards.java
@@ -1,20 +1,25 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.ComponentOrientation;
+import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
@@ -22,11 +27,15 @@ public class BaseballCards extends JPanel {
 
 	private final int WIDTH =600;
 	private final int HEIGHT = 400;
+	private final String TITLE = "Baseball Cards";
 	
 	private FlowLayout buttonLayout = new FlowLayout();
 	private JButton printPlayers, insertPlayers;
 	private JTextField playerInput,teamInput,ageInput,numberInput;
-	private JLabel playerName,teamName,age,number;
+	private JTextArea addingPlayers,playersInfo;
+	private JLabel playerName,teamName,age,number,empty,playersCounter, playersOutput;
+	
+	private Container container;
 	private EmptyBorder border;
 	private EmptyBorder border1;
 	private EmptyBorder border2;
@@ -38,11 +47,24 @@ public class BaseballCards extends JPanel {
 	private BaseballPlayers baseballPlayer;
 	private Team team;
 			
+
 		public BaseballCards() {
 			baseballPlayer = new BaseballPlayers();
 			baseballPlayer.checkNumber();
 			team = new Team();
 			team.addTeamPlayer(baseballPlayer);
+			
+			//JPanel setup
+			JPanel jPanel2 = new JPanel();
+			jPanel2.setBackground(Color.BLACK);
+			jPanel2.setLayout(new BoxLayout(jPanel2,  BoxLayout.PAGE_AXIS));
+			jPanel2.setOpaque(false);
+			
+			JPanel jPanel3 = new JPanel();
+			jPanel3.setBackground(Color.BLACK);
+			jPanel3.setLayout(new BoxLayout(jPanel3,  BoxLayout.PAGE_AXIS));
+			jPanel3.setOpaque(false);
+		
 		
 			//Initializing the jframe
 			JFrame window= new JFrame();
@@ -58,24 +80,56 @@ public class BaseballCards extends JPanel {
 			border = new EmptyBorder(5, 6, 5, 20);
 			border1 = new EmptyBorder(5, 6, 5, 8);
 			border2 = new EmptyBorder(5, 6, 5, 8);
-			border3 = new EmptyBorder(5, 100, 5, 8);
-			border4 = new EmptyBorder(5, 6, 5, 8);
+			border3 = new EmptyBorder(5, 300, 5, 8);
+			border4 = new EmptyBorder(5, 178, 5, 8);
 			
+			//Initializing JTextArea
+			addingPlayers = new JTextArea(10,23);
+			addingPlayers.setLineWrap(true);
+			addingPlayers.setWrapStyleWord(true);
+			addingPlayers.setEditable(false);
 			
+			playersInfo = new JTextArea(10,23);
+			playersInfo.setLineWrap(true);
+			playersInfo.setWrapStyleWord(true);
+			playersInfo.setEditable(false);
+
+			//Initializing JScroll
+			JScrollPane scroll = new JScrollPane(addingPlayers);
+			JScrollPane scroll2 = new JScrollPane(playersInfo);
+			
+		
+			//JLabels
 			playerName = new JLabel("Player's name: ");
 			playerName.setForeground(Color.WHITE);
 			teamName = new JLabel("Team's name: ");
-			
 			teamName.setBorder(border);
 			teamName.setForeground(Color.WHITE);
 			age = new JLabel("Player's age: ");
 			age.setForeground(Color.WHITE);
+			empty = new JLabel();
+			empty.setForeground(Color.BLACK);
 			number = new JLabel("Player's number ");
 			number.setForeground(Color.WHITE);
+			playersCounter = new JLabel("Players");
+			playersCounter.setForeground(Color.WHITE);
+			playersCounter.setOpaque(false);
+			playersOutput = new JLabel("Player's Info");
+			playersOutput.setForeground(Color.WHITE);
+			playersOutput.setOpaque(false);
+			
+			//JTextFiels
 			playerInput = new JTextField(10);
 			teamInput = new JTextField(10);
 			ageInput = new JTextField(10);
 			numberInput = new JTextField(10);
+			
+//			//Container
+//			container = new Container();
+//			container.add(playersCounter,addingPlayers);
+//			container.setVisible(true);
+			
+			//JButtons
 			printPlayers = new JButton("Player's info");
 			printPlayers.setBorder(border3);
 			printPlayers.setBorder(BorderFactory.createCompoundBorder(
@@ -106,7 +160,8 @@ public class BaseballCards extends JPanel {
 				}
 				catch(Exception E) {
 					
-					System.out.println("Try filling every box");
+					JOptionPane.showMessageDialog(null, "Try filling up all the boxes", "Missing boxes",JOptionPane.ERROR_MESSAGE);
+					
 					
 				}
 				}
@@ -116,25 +171,33 @@ public class BaseballCards extends JPanel {
 			add(playerName);
 			playerName.setBorder(border1);
 			add(playerInput);
-			
 			add(teamName);
 			teamName.setBorder(border2);
 			add(teamInput);
-			
 			add(age);
 			age.setBorder(border);
 			add(ageInput);
-			
 			add(number);
 			add(numberInput);
-
-			add(printPlayers);
+			add(empty);
+			empty.setBorder(border4);
 			add(insertPlayers);
+			add(printPlayers);
 			
+			jPanel2.add(playersCounter);
+//			playersCounter.setBorder(border3);
+			jPanel2.add(scroll,addingPlayers);
+			jPanel3.add(playersOutput);
+			jPanel3.add(scroll2,playersInfo);
+			add(jPanel2);
+			add(jPanel3);
 			//the JFrame setup
 			window.setSize(WIDTH,HEIGHT);
 			window.setBackground(Color.BLACK);
-			window.getContentPane().add(this, BorderLayout.CENTER);
+			window.setTitle(TITLE);
+			window.getContentPane().add(this,BorderLayout.CENTER);
+	
+//			window.add(jPanel2);
 		
 			
 			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
